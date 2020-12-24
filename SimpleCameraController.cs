3e0a879d23790438cdf60acfb17a3492ed8d83cc
GodLoveTom <76538241+GodLoveTom@users.noreply.github.com>
2020-12-24 11:23:57 +0800

    Update SimpleCameraController.cs
    so ga

--- SimpleCameraController.cs
+++ SimpleCameraController.cs
@@ -12,6 +12,7 @@ namespace UnityTemplateProjects
             public float x;
             public float y;
             public float z;
+	    public int ccc;
 
             public void SetFromTransform(Transform t)
             {
